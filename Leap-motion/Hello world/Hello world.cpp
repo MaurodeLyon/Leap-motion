9fc246bd67dc1b9965b1b5fcb1b308bf3153ee45
Mauro de Lyon <maurodelyon@gmail.com>
2016-05-09 09:56:50 +0200

    added gesture recognition

--- Leap-motion/Hello world/Hello world.cpp
+++ Leap-motion/Hello world/Hello world.cpp
@@ -31,11 +31,44 @@ void SampleListener::onFrame(const Controller& controller)
 	auto pointables = frame.pointables();
 	auto fingers = frame.fingers();
 	auto tools = frame.tools();
-	std::cout << "hands: " << hands.count() << " pointables: " << pointables.count() << " fingers: " << fingers.count() << " tools: " << tools.count() << std::endl;
-	
+	//display hand info
+	//std::cout << "hands: " << hands.count() << " pointables: " << pointables.count() << " fingers: " << fingers.count() << " tools: " << tools.count() << std::endl;
 
-	//std::cout << "Frame id: " << frame.id() << ", timestamp: " << frame.timestamp() << ", hands: " << frame.hands().count() << ", fingers: " << frame.fingers().count() << ", tools: " << frame.tools().count() << ", gestures: " << frame.gestures().count() << std::endl;
-	/*for each(Hand hand in frame.hands())
+	//display gestures
+	auto circle = 0;
+	auto swipe = 0;
+	auto keytap = 0;
+	auto screentap = 0;
+	Leap::GestureList gestures = frame.gestures();
+	for (Leap::GestureList::const_iterator gl = gestures.begin(); gl != frame.gestures().end(); gl++)
+	{
+		switch ((*gl).type()) {
+		case Leap::Gesture::TYPE_CIRCLE:
+			//Handle circle gestures
+			circle++;
+			break;
+		case Leap::Gesture::TYPE_KEY_TAP:
+			//Handle key tap gestures
+			keytap++;
+			break;
+		case Leap::Gesture::TYPE_SCREEN_TAP:
+			//Handle screen tap gestures
+			screentap++;
+			break;
+		case Leap::Gesture::TYPE_SWIPE:
+			//Handle swipe gestures
+			swipe++;
+			break;
+		default:
+			//Handle unrecognized gestures
+			break;
+		}
+	}
+	std::cout << "Circle: " << circle << "\t swipe: " << swipe << "\t keytap: " << keytap << "\t screentap: " << screentap << std::endl;
+	
+	//displayframe info
+	/*std::cout << "Frame id: " << frame.id() << ", timestamp: " << frame.timestamp() << ", hands: " << frame.hands().count() << ", fingers: " << frame.fingers().count() << ", tools: " << frame.tools().count() << ", gestures: " << frame.gestures().count() << std::endl;
+	for each(Hand hand in frame.hands())
 	{
 		for each(Finger finger in hand.fingers()) {
 			std::cout << finger.TYPE_MIDDLE << std::endl;
